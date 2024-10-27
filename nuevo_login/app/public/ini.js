ed8dd818186fec0723ae70d8a005513391f385a3
jesus <110626371+malitokira99@users.noreply.github.com>
2024-10-27 08:47:48 -0500

    compentarios para ini.js

--- nuevo_login/app/public/ini.js
+++ nuevo_login/app/public/ini.js
@@ -1,4 +1,8 @@
-document.getElementsByTagName("button")[0].addEventListener("click",()=>{
-    document.cookie ='jwt=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-    document.location.href = "/"
-  })
+// Añadimos un evento de escucha al primer botón en el documento
+document.getElementsByTagName("button")[0].addEventListener("click", () => {
+  // Borramos la cookie 'jwt' estableciendo su fecha de expiración en el pasado
+  document.cookie = 'jwt=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+  
+  // Redirigimos al usuario a la página principal
+  document.location.href = "/";
+});
